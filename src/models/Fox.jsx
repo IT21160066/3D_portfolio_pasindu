b33d236cbc4317696b458a0d72a3dee88bacbd19
Pasindu Perakum <it21160066my.sliit.lk>
2023-12-19 11:45:37 +0530

    hello

--- src/models/Fox.jsx
+++ src/models/Fox.jsx
@@ -9,7 +9,7 @@ Title: Fox
 import React, { useRef } from "react";
 import { useGLTF, useAnimations } from "@react-three/drei";
 import scene from "../assets/3d/fox.glb";
-import { useEffect } from "react";
+import { useEffect } from "react"; //hello
 
 const Fox = ({ currentAnimation, ...props }) => {
   const group = useRef();
